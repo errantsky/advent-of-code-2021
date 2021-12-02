9350cf22e518d43a930dd4a766bb38253fd3dcdd
errantsky <dev@errantsky.dev>
2021-12-01 20:15:30 -0500

    refactor: Replace for loops with `map` and `fold`

--- src/rust/day1_sonar_sweep.rs
+++ src/rust/day1_sonar_sweep.rs
@@ -1,30 +1,28 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-/// ToDo: rewrite in a more idiomatic and functional way
-/// ToDo: solve in a single pass
-
 const SAMPLE_PATH: &str = "/Users/sep/CLionProjects/aoc-2021/src/test_files/day1_part1_sample.txt";
 const SUBMISSION_PATH: &str =
     "/Users/sep/CLionProjects/aoc-2021/src/test_files/day1_part1_submission.txt";
 
 fn part1(numbers: Vec<usize>) -> usize {
-    let mut prev = usize::MAX;
-    let mut acc = 0;
-    for number in numbers {
-        if number > prev {
-            acc += 1;
+    let mut ni = numbers.into_iter();
+    let mut prev = ni.next().unwrap();
+    ni.fold(0, move |acc, n| {
+        if prev < n {
+            prev = n;
+            acc + 1
+        } else {
+            prev = n;
+            acc
         }
-        prev = number;
-    }
-    acc
+    })
 }
 
 fn part2(numbers: Vec<usize>) -> usize {
-    let mut triple_sums = Vec::new();
-    for i in 0..(numbers.len() - 3 + 1) {
-        triple_sums.push(numbers[i..(i + 3)].iter().sum())
-    }
+    let triple_sums = (0..(numbers.len() - 3 + 1))
+        .map(move |i| numbers[i..(i + 3)].iter().sum())
+        .collect();
     part1(triple_sums)
 }
 
