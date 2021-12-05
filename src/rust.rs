0a84898aabf38d2c45c07446df9c2abba836f210
errantsky <dev@errantsky.dev>
2021-12-05 17:01:09 -0500

    Add the baseline Day 5 solutions in Python
    Space complexity is `O(n^2)` and time complexity is also `O(n^2)`. I review my computational
    geometry notes for a proper line intersection
    solution.

--- src/rust.rs
+++ src/rust.rs
@@ -2,3 +2,4 @@ mod day1_sonar_sweep;
 mod day2_dive;
 mod day3_binary_diagnostic;
 mod day4_giant_squid;
+mod day5_hydrothermal_venture;
---
+++ src/rust/day5_hydrothermal_venture.rs
@@ -0,0 +1,41 @@
+use std::fs::File;
+
+const SAMPLE_PATH: &str = "/Users/sep/CLionProjects/aoc-2021/src/test_files/day5_sample.txt";
+const SUBMISSION_PATH: &str =
+    "/Users/sep/CLionProjects/aoc-2021/src/test_files/day5_submission.txt";
+
+fn part1(path: &str) -> usize {}
+
+// fn part2(path: &str) -> usize {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::read_to_string;
+
+    #[test]
+    fn test_sample_part1() {
+        let res = part1(SAMPLE_PATH);
+        assert_eq!(res, 5);
+    }
+
+    #[test]
+    fn test_submission_part1() {
+        let res = part1(SUBMISSION_PATH);
+        assert_eq!(res, 16674);
+        // println!("day 5, Part 1: {}", res);
+    }
+
+    #[test]
+    fn test_sample_part2() {
+        let res = part2(SAMPLE_PATH);
+        assert_eq!(res, 1924);
+    }
+
+    #[test]
+    fn test_submission_part2() {
+        let mut res = part2(SUBMISSION_PATH);
+        assert_eq!(res, 7075);
+        // println!("day 5, Part 2: {}", res);
+    }
+}
